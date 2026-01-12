9863aeaceebb96ee4f211ca9d6749904141d9bae
Balavignesh S <99158572+Bala-5657@users.noreply.github.com>
2026-01-12 12:14:39 +0530

    Added requirements.txt

--- healthcare_chatbot/settings.py
+++ healthcare_chatbot/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = "healthcare_chatbot.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': str(os.getenv('DATABASE_NAME')),
-        'USER': str(os.getenv('DATABASE_USER')),
-        'PASSWORD': str(os.getenv('DATABASE_PASSWORD')),
-        'HOST': str(os.getenv('DATABASE_HOST')),   # Or the IP address of the database
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': str(os.getenv('DATABASE_NAME')),
+#         'USER': str(os.getenv('DATABASE_USER')),
+#         'PASSWORD': str(os.getenv('DATABASE_PASSWORD')),
+#         'HOST': str(os.getenv('DATABASE_HOST')),   # Or the IP address of the database
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
